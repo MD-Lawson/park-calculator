d37847b22fb06d71a4cb8c23c606fce7f96fb084
matthewl <matthew.lawson1@vodafone.com>
2019-05-02 14:54:00 +0100

    Clean up unnecessary import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Navbar from "./components/Navbar";
 import Title from "./components/Title";
 import Home from "./Home/Home";
