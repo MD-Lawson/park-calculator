325abc213e11e953a695ada5320ddcedeaee7179
matthewl <matthew.lawson1@vodafone.com>
2019-05-02 15:27:23 +0100

    Added fontawesome. Refactored howManyCard.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
 import Navbar from "./components/Navbar";
 import Title from "./components/Title";
 import Home from "./Home/Home";
 import './App.css';
 
-
+library.add(faPlus)
+library.add(faMinus)
 function App() {
   return (
     <Container className="App">
--- src/Home/howManyCard.js
+++ src/Home/howManyCard.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { Button, Card } from 'semantic-ui-react';
+//import { Button, Card } from 'semantic-ui-react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 function buttonClick(callBack, plus, adultCard) {
     callBack(adultCard, plus);
@@ -11,31 +14,34 @@ function HowMany(props) {
     var adultCard = true;
     if (props.numChild != null) adultCard = false;
     return (
-        <Card style={{
+        <Card bg="light" style={{
+            'width': '50%',
             'marginLeft': 'auto',
             'marginRight': 'auto'
         }}>
-            <Row>
-                <Col sm={12}>
-                    <p>{props.title}</p>
-                </Col>
-            </Row>
-            <Row>
-                <Col sm={5} style={{
-                    'paddingRight': '0%',
-                    'marginTop': '1%',
-                    'marginBottom': '1%'
-                }}>
-                    <Button style={{ 'float': 'right' }} circular icon='minus' size='huge' onClick={() => buttonClick(props.handleClick, false, adultCard)} />
-                </Col>
-                <Col sm={2} style={{ 'padding': '0% 0%' }}><p style={{ 'fontSize': '2.5rem' }}>{adultCard ? props.numAdults : props.numChild}</p></Col>
-                <Col sm={5} style={{
-                    'paddingLeft': '0%',
-                    'marginTop': '1%',
-                    'marginBottom': '1%'
-                }}><Button style={{ 'float': 'left' }} circular icon='plus' size='huge' onClick={() => buttonClick(props.handleClick, true, adultCard)} />
-                </Col>
-            </Row>
+            <Card.Body>
+                <Row>
+                    <Col sm={12}>
+                        <p>{props.title}</p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm={5} style={{
+                        'paddingRight': '0%',
+                        'marginTop': '1%',
+                        'marginBottom': '1%'
+                    }}>
+                        <Button variant='secondary' style={{ 'float': 'right' }} onClick={() => buttonClick(props.handleClick, false, adultCard)} ><FontAwesomeIcon icon='plus' /> </Button>
+                    </Col>
+                    <Col sm={2} style={{ 'padding': '0% 0%' }}><p style={{ 'fontSize': '2.5rem' }}>{adultCard ? props.numAdults : props.numChild}</p></Col>
+                    <Col sm={5} style={{
+                        'paddingLeft': '0%',
+                        'marginTop': '1%',
+                        'marginBottom': '1%'
+                    }}><Button variant='secondary' style={{ 'float': 'left' }} onClick={() => buttonClick(props.handleClick, true, adultCard)}> <FontAwesomeIcon icon='minus' /> </Button>
+                    </Col>
+                </Row>
+            </Card.Body>
         </Card>
     );
 }
