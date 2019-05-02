8c438af6f05618e033cf124a80f246a744d6ef88
matthewl <matthew.lawson1@vodafone.com>
2019-05-02 14:36:02 +0100

    Modal Stuff. Refactor to full bootstrap from here.

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import { Button, Modal, Icon } from 'semantic-ui-react';
 import HowMany from './howManyCard';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -10,8 +11,10 @@ class Home extends React.Component {
         this.state = {
             numAdults: 0,
             numChild: 0,
+            modalOpen: false,
         }
         this.handlePlusorMinusClick = this.handlePlusorMinusClick.bind(this);
+        this.handleModal = this.handleModal.bind(this);
     }
 
     handlePlusorMinusClick(adults, plus) {
@@ -20,21 +23,52 @@ class Home extends React.Component {
         if (adults) {
             if (plus) this.setState({ numAdults: numAdults + 1 })
             if (!plus && numAdults > 0) this.setState({ numAdults: numAdults - 1 })
-        }else{
+        } else {
             if (plus) this.setState({ numChild: numChild + 1 })
             if (!plus && numChild > 0) this.setState({ numChild: numChild - 1 })
         }
     }
 
+    handleModal() {
+        this.setState({
+            modalOpen: !this.state.modalOpen
+        })
+    }
+
     render() {
         return (
             <Container>
-                <Row>
+                <Row style={{ 'marginBottom': '5%' }}>
                     <Col>
                         <HowMany title='Number of Adults' numAdults={this.state.numAdults} handleClick={this.handlePlusorMinusClick} />
                     </Col>
                     <Col>
                         <HowMany title='Number of Children' numChild={this.state.numChild} handleClick={this.handlePlusorMinusClick} />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm={3}>
+                        <Modal
+                            trigger={<Button circular icon='plus' size='massive' style={{ 'float': 'right' }} onClick={this.handleModal} />}
+                            open={this.state.modalOpen}
+                            size='small'>
+                             <Modal.Header >Select a park </Modal.Header>
+                            <Modal.Description>
+                                <h3>Test</h3>
+                            </Modal.Description>
+                            <Modal.Actions>
+                                <Button color='green' onClick={this.handleModal}>
+                                    <Icon name='checkmark' /> Confirm
+                        </Button>
+                            </Modal.Actions>
+                        </Modal>
+                    </Col>
+                    <Col>
+                        <p style={{
+                            'float': 'left',
+                            'padding': '10px 0',
+                            'fontSize': '2rem'
+                        }}>Add New Park</p>
                     </Col>
                 </Row>
             </Container>
